d624d98088a86e7573809493b6d08c78f4d74ea3
Pramila Khanal <pramilakhanal@Pramilas-MBP.lan>
2018-02-24 22:13:23 -0700

    new connection added

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
